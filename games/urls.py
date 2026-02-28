b0effcda3700bea4542c63a9bf6c86aece132e30
Миха <aleksejruzavin56@gmail.com>
2026-02-28 21:25:47 +0800

    1 commit

--- games/urls.py
+++ games/urls.py
@@ -4,3 +4,4 @@ from .views import game_list, top_games, game_detail
 urlpatterns = [path("top_games", top_games, name='top_games'),
                 path("game_detail/<int:id>", game_detail, name='game_detail'),
                path("", game_list, name='game_list'),]
+
